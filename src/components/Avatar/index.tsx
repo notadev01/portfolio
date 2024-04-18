b294978236474e9cafff9c4283e0f8e1902fcb2e
notadev01 <notadev01@email.com>
2024-04-18 18:24:45 -0300

    Estilizar a Sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/notadev01.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/notadev01.png" />
-    <Titulo fontSize={20}>notadev01</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>notadev01</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        notadev01
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
